b04940fa3ed2724f1ca16096c224e634bd209809
hanmiyoo10-alt <hanmiyoo10@gmail.com>
2026-08-13 01:42:40 +0900

    Add generic RisuAI update template

---
+++ plugins/_template/latest.js
@@ -0,0 +1,10 @@
+//@name plugin_update_template
+//@display-name Plugin Update Template
+//@version 0.0.1
+//@api 3.0
+//@update-url https://raw.githubusercontent.com/hanmiyoo10-alt/-/main/plugins/_template/latest.js
+
+(() => {
+  'use strict';
+  console.log('[Plugin Update Template] loaded');
+})();
